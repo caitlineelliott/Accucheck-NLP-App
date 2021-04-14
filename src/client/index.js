0e50797c440758991c339b2dee679a010871e835
Caitlin Elliott <ceelliott90@gmail.com>
2021-04-14 16:12:32 -0400

    added header styles

--- src/client/index.js
+++ src/client/index.js
@@ -3,5 +3,6 @@ import { handleSubmit } from './js/handleSubmit'
 import './styles/resets.scss'
 import './styles/base.scss'
 import './styles/form.scss'
+import './styles/header.scss'
 
 console.log(handleSubmit);
