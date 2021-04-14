fc290a161f82dc193ad8a8014e04fe58ed29406f
Caitlin Elliott <ceelliott90@gmail.com>
2021-04-14 16:04:52 -0400

    finalized ui updating function

--- src/client/js/updateUI.js
+++ src/client/js/updateUI.js
@@ -5,12 +5,11 @@ function updateUI(data) {
     // document.querySelector('.form-results').classList.add('display-on');
 
     // add data to results table
-    document.querySelector('#results').innerHTML = data.scoreTag;
-    // document.querySelector('#score-tag').innerHTML = data.scoreTag;
-    // document.querySelector('#agreement').innerHTML = data.agreement;
-    // document.querySelector('#subjectivity').innerHTML = data.subjectivity;
-    // document.querySelector('#confidence').innerHTML = data.confidence;
-    // document.querySelector('#irony').innerHTML = data.irony;
+    document.querySelector('#score-tag').innerHTML = data.scoreTag;
+    document.querySelector('#agreement').innerHTML = data.agreement;
+    document.querySelector('#subjectivity').innerHTML = data.subjectivity;
+    document.querySelector('#confidence').innerHTML = data.confidence;
+    document.querySelector('#irony').innerHTML = data.irony;
 }
 
 export { updateUI }
