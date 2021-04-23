1cb34f3db49a47abb33f636f07a10b2b65befe41
Caitlin Elliott <ceelliott90@gmail.com>
2021-04-23 12:57:54 -0400

    added mobile hamburger nav

--- src/client/index.js
+++ src/client/index.js
@@ -1,4 +1,9 @@
+import { activeNav } from './js/handleSubmit'
+import { restingNav } from './js/handleSubmit'
 import { handleSubmit } from './js/handleSubmit'
+import { checkURL } from './js/checkURL'
+import { sendData } from './js/sendData'
+import { updateUI } from './js/updateUI'
 
 import './styles/resets.scss'
 import './styles/base.scss'
--- src/client/js/handleSubmit.js
+++ src/client/js/handleSubmit.js
@@ -32,3 +32,41 @@ async function handleSubmit(event) {
 }
 
 export { handleSubmit }
+
+// variables for mobile nav
+const hamIcon = document.querySelector('.hamburger-icon');
+const closeBtn = document.querySelector('.close-btn');
+
+/**
+ * @description toggles mobile nav active & resting states
+ */
+
+const restingNav = () => {
+    hamIcon.classList.remove('hamburger-none');
+    document.querySelector('nav').classList.add('nav');
+    document.querySelector('nav').classList.remove('mobile-nav');
+    closeBtn.classList.remove('close-btn-active');
+    document.querySelector('.nav-list').innerHTML = '';
+};
+
+const activeNav = () => {
+    hamIcon.classList.add('hamburger-none');
+
+    const navItems = document.getElementsByClassName('nav-links');
+    document.querySelector('.nav-list').innerHTML =
+        `${navItems[0].outerHTML}<br>
+        ${navItems[1].outerHTML}<br>
+        ${navItems[2].outerHTML}<br>
+        ${navItems[3].outerHTML}<br>`;
+
+    document.querySelector('nav').classList.add('mobile-nav');
+    document.querySelector('nav').classList.remove('nav');
+    closeBtn.classList.add('close-btn-active');
+    closeBtn.addEventListener('click', restingNav);
+    document.querySelector('.nav-list').addEventListener('click', restingNav);
+};
+
+hamIcon.addEventListener('click', activeNav);
+
+export { restingNav }
+export { activeNav }
