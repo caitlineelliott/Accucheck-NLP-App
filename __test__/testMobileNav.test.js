46e983b51cd5bfd04cc71dddf15316d9599aabe8
Caitlin Elliott <ceelliott90@gmail.com>
2021-04-29 12:35:40 -0400

    separated mobileNav functions and added jest test

---
+++ __test__/testMobileNav.test.js
@@ -0,0 +1,5 @@
+const mobileNav = require('../src/client/js/mobileNav');
+
+test('Mobile nav successfully builds', () => {
+    expect(mobileNav).toBeDefined();
+});
--- src/client/index.js
+++ src/client/index.js
@@ -4,6 +4,8 @@ import { handleSubmit } from './js/handleSubmit'
 import { checkURL } from './js/checkURL'
 import { sendData } from './js/sendData'
 import { updateUI } from './js/updateUI'
+import { restingNav } from './js/mobileNav'
+import { activeNav } from './js/mobileNav'
 
 import './styles/resets.scss'
 import './styles/base.scss'
