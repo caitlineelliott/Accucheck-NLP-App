4b426d99435936152d420a2dc79e8c028c3253d5
Caitlin Elliott <ceelliott90@gmail.com>
2021-04-29 12:53:16 -0400

    fixed typos in import statemenst in index.js

--- src/client/index.js
+++ src/client/index.js
@@ -1,11 +1,9 @@
-import { activeNav } from './js/handleSubmit'
-import { restingNav } from './js/handleSubmit'
+import { activeNav } from './js/mobileNav'
+import { restingNav } from './js/mobileNav'
 import { handleSubmit } from './js/handleSubmit'
 import { checkURL } from './js/checkURL'
 import { sendData } from './js/sendData'
 import { updateUI } from './js/updateUI'
-import { restingNav } from './js/mobileNav'
-import { activeNav } from './js/mobileNav'
 
 import './styles/resets.scss'
 import './styles/base.scss'
